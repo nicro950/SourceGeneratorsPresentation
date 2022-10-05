e7740b7cb4877e30da0f347f0ae5efc423a2e00e
Nicolas Fløysvik <nico-floysvik@hotmail.com>
2022-10-05 22:11:29 +0200

    Complete Source generator

--- src/NicroWare.Pro.DemoSourceGen/HelloSourceGenerator.cs
+++ src/NicroWare.Pro.DemoSourceGen/HelloSourceGenerator.cs
@@ -1,18 +1,85 @@
 ﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace NicroWare.Pro.DemoSourceGen;
 
 [Generator]
 public class HelloSourceGenerator : ISourceGenerator
 {
-	public void Initialize(GeneratorInitializationContext context)
-	{
-		// Initialize code here
-	}
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        // Initialize code here
+        // Debugger.Launch();
+        
+        context.RegisterForSyntaxNotifications(() => new NotifiSyntaxReceiver());
+    }
 
-	public void Execute(GeneratorExecutionContext context)
-	{
-		// Generating extra code here
-	}
+    public void Execute(GeneratorExecutionContext context)
+    {
+
+        if (!(context.SyntaxReceiver is NotifiSyntaxReceiver nsr))
+            return;
+
+        string ToPascalCase(string name)
+            => name.Substring(0, 1).ToUpper() + name.Substring(1);
+
+		foreach (var cls in nsr.Classes)
+        {
+            var sementicModel = context.Compilation.GetSemanticModel(cls.SyntaxTree);
+            var symbol = sementicModel.GetDeclaredSymbol(cls) as ITypeSymbol;
+
+            if (symbol == null)
+                continue;
+
+            var mem = symbol.GetMembers();
+
+            var allFields = mem.OfType<IFieldSymbol>();
+
+            var source = $$"""
+                using System;
+                using System.ComponentModel;
+                using System.Runtime.CompilerServices;
+
+                namespace {{symbol.ContainingNamespace.ToDisplayString()}};
+
+                public partial class {{symbol.Name}}
+                {
+                    public event PropertyChangedEventHandler? PropertyChanged;
+
+                    private void OnPropertyChanged([CallerMemberName] string fieldName = null!)
+                    {
+                        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(fieldName));
+                    }
+                {{ string.Join(Environment.NewLine, allFields.Select(x => $"    public {x.Type.ToString()} {ToPascalCase(x.Name)} {{ get => {x.Name}; set  {{ {x.Name} = value; OnPropertyChanged(); }}}}")) }}
+
+                    public object GetValueForProperty(string name)
+                        => name switch {
+                {{ string.Join(Environment.NewLine, allFields.Select(x => $"            \"{ToPascalCase(x.Name)}\" => this.{x.Name},"))}}
+                            _ => null,
+                        };
+                    
+                }
+                """;
+
+            context.AddSource(symbol.Name + ".g.cs", source);
+        }
+    }
+}
+
+public class NotifiSyntaxReceiver : ISyntaxReceiver
+{
+    public List<ClassDeclarationSyntax> Classes { get; } = new();
+    public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+    {
+        if (syntaxNode is ClassDeclarationSyntax cds && (cds.BaseList?.Types.Any(a => a.ToString() == "INotifyPropertyChanged") ?? false))
+        {
+            Classes.Add(cds);
+        }
+    }
 }
--- src/NicroWare.Pro.SourceGenerators/Program.cs
+++ src/NicroWare.Pro.SourceGenerators/Program.cs
@@ -1,2 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace NicroWare.Pro.SourceGenerators;
+
+public partial class Program
+{
+    public static void Main()
+    {
+        HelloFrom("Somewhere");
+
+        var person = new Person("First", "Last");
+
+        person.PropertyChanged += Person_PropertyChanged  ;
+
+        person.FirstName = "bob";
+		person.Age = 25;
+
+	}
+
+    private static void Person_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        
+
+		Console.WriteLine($"Someone changed {e.PropertyName} new value is '{(sender as Person).GetValueForProperty(e.PropertyName)}'!");
+    }
+
+    static partial void HelloFrom(string name);
+}
+
+public partial class Person : INotifyPropertyChanged
+{
+    public Person(string firstName, string lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    private string firstName;
+    private string? middleName;
+    private string lastName;
+
+    private int age;
+}
