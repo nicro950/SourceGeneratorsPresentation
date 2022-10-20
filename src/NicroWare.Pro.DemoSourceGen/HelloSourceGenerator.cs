106ef2513341844fad61b78730ae8b0be415c46f
Nicolas Fløysvik <nicolas.floysvik@bouvet.no>
2022-10-20 13:25:09 +0200

    Finished version

--- src/NicroWare.Pro.DemoSourceGen/HelloSourceGenerator.cs
+++ src/NicroWare.Pro.DemoSourceGen/HelloSourceGenerator.cs
@@ -1,18 +1,66 @@
 ﻿
+using System.Diagnostics;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace NicroWare.Pro.DemoSourceGen;
 
 [Generator]
 public class HelloSourceGenerator : ISourceGenerator
 {
-	public void Initialize(GeneratorInitializationContext context)
-	{
-		// Initialize code here
-	}
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        // Debugger.Launch();
+        context.RegisterForSyntaxNotifications(() => new NotifiSyntaxReceiver());
+    }
 
-	public void Execute(GeneratorExecutionContext context)
-	{
-		// Generating extra code here
-	}
+    public void Execute(GeneratorExecutionContext context)
+    {
+        if (context.SyntaxReceiver is not NotifiSyntaxReceiver nsr)
+            return;
+
+        foreach (var cls in nsr.Classes)
+        {
+            var sementicModel = context.Compilation.GetSemanticModel(cls.SyntaxTree);
+            var symbol = sementicModel.GetDeclaredSymbol(cls) as ITypeSymbol;
+
+            var mem = symbol.GetMembers();
+
+            var allFields = mem.OfType<IFieldSymbol>();
+
+            var source = $$"""
+                using System;
+                using System.ComponentModel;
+                using System.Runtime.CompilerServices;
+
+                namespace {{symbol.ContainingNamespace.ToDisplayString()}};
+
+                public partial class {{symbol.Name}}
+                {
+                    public event PropertyChangedEventHandler? PropertyChanged;
+
+                    private void OnPropertyChanged([CallerMemberName] string name = null!)
+                    {
+                        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+                    }
+                {{string.Join(Environment.NewLine, allFields.Select(x => $"    public {x.Type.ToString()} {x.Name.Substring(0, 1).ToUpper()}{x.Name.Substring(1)} {{ get => {x.Name}; set  {{ {x.Name} = value; OnPropertyChanged(); }}}}"))}}
+                }
+                """;
+
+            context.AddSource(symbol.Name + ".g.cs", source);
+        }
+    }
+}
+
+
+public class NotifiSyntaxReceiver : ISyntaxReceiver
+{
+    public List<ClassDeclarationSyntax> Classes { get; } = new();
+    public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+    {
+        if (syntaxNode is ClassDeclarationSyntax cds && (cds.BaseList?.Types.Any(a => a.ToString() == "INotifyPropertyChanged") ?? false))
+        {
+            Classes.Add(cds);
+        }
+    }
 }
--- src/NicroWare.Pro.SourceGenerators/Program.cs
+++ src/NicroWare.Pro.SourceGenerators/Program.cs
@@ -1,2 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace NicroWare.Pro.SourceGenerators;
+
+public static partial class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+
+        var person = new Person("a", "b");
+
+        person.PropertyChanged += Person_PropertyChanged;
+
+        person.FirstName = "b";
+    }
+
+    private static void Person_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        Console.WriteLine(e.PropertyName);
+    }
+}
+
+public partial class Person : INotifyPropertyChanged
+{
+
+    public Person(string firstName, string lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    private string firstName;
+    private string lastName;
+    private int age;
+    private string? middleName;
+}
